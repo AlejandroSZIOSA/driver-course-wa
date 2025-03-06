59e544d28112ddf71fc98ea85daa74788a8da7df
AlejandroSZIOSA <ggato33@gmail.com>
2025-03-06 10:56:41 +0100

    home page footer

--- src/components/main-footer.js
+++ src/components/main-footer.js
@@ -1,4 +1,28 @@
 import classes from "@/styles/components/MainFooter.module.css";
+import Image from "next/image";
 export default function MainFooter() {
-  return <div className={classes.container}>Main footer</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.innerContainer}>
+        <div>
+          <Image
+            src="/svg/icons/phone.svg"
+            width={25}
+            height={25}
+            alt="LockIcon"
+          />
+          <p>: XXXXXXXXXXXX</p>
+        </div>
+        <div>
+          <Image
+            src="/svg/icons/epost.svg"
+            width={25}
+            height={25}
+            alt="LockIcon"
+          />
+          <p>: XXXXXX@XXXXXX.XX</p>
+        </div>
+      </div>
+    </div>
+  );
 }
