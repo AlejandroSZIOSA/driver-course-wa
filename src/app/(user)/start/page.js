47d7c33f2dcdf0eaa2ced63bec8fe8d601a3c2c3
AlejandroSZIOSA <ggato33@gmail.com>
2025-03-10 16:05:59 +0100

    randomized correct answer answer

--- src/app/(user)/start/page.js
+++ src/app/(user)/start/page.js
@@ -4,43 +4,88 @@ import { useRouter } from "next/navigation";
 import { useQuestions } from "@/context/QuestionsContext";
 import classes from "@/styles/pages/Start.module.css";
 import Image from "next/image";
+import PrimaryButton from "@/components/primary-button";
 
+/* const TEST_QUESTIONS = ["ans1", "ans2", "ans3", "correct"]; */
 export default function StartPage() {
+  /*  debugger; */
   const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [finalUserAnswers, setFinalUserAnswers] = useState([]);
 
+  const [selectedQuestion, setSelectedQuestion] = useState();
   const { questions } = useQuestions();
+
   let LAST_INDEX = questions.length - 1;
 
   useEffect(() => {
     if (currentIndex > LAST_INDEX) {
       router.push("/summary");
+    } else {
+      randomizeFinalAnswers(addCorrectAnswers(currentIndex));
     }
   }, [currentIndex]);
 
+  function addCorrectAnswers(index) {
+    let { wrongAnswers, answer } = questions[index];
+    let finalAnswers = new Array();
+    finalAnswers = [...wrongAnswers, answer];
+    /* console.log(finalAnswers); */
+    return finalAnswers;
+  }
+
+  function randomizeFinalAnswers(preAnswers) {
+    let randomAnswers = [];
+    for (let i = preAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [preAnswers[i], preAnswers[j]] = [preAnswers[j], preAnswers[i]]; // Swap elements
+    }
+    randomAnswers = preAnswers;
+    setFinalUserAnswers(randomAnswers);
+
+    /* console.log(finalUserAnswers); */
+  }
+
   if (currentIndex <= LAST_INDEX) {
-    console.log(questions);
+    /* console.log(questions); */
     /* debugger; */
     return (
       <main className={classes.main}>
         <section>
           <h1>Question {currentIndex + 1}</h1>
-          <div>
+          <div className={classes.imageContainer}>
             <Image
               src={questions[currentIndex].imageUrl}
-              width={400}
-              height={500}
+              width={380}
+              height={430}
               alt="QuestionImage"
               priority={true}
             />
-
             <p>{questions[currentIndex].description}</p>
+            <hr></hr>
           </div>
-          <div>
-            <h2>{questions[currentIndex].description}</h2>
-            <button onClick={() => setCurrentIndex(currentIndex + 1)}>
+          <div className={classes.questionsContainer}>
+            <h2>{questions[currentIndex].question}</h2>
+            <div className={classes.answersContainer}>
+              <h3>Choose your answer</h3>
+              {finalUserAnswers.map((option) => (
+                <label key={option}>
+                  <input
+                    type="radio"
+                    value={option}
+                    checked={selectedQuestion === option}
+                    onChange={(e) => setSelectedQuestion(e.target.value)}
+                  />
+                  {option}
+                </label>
+              ))}
+
+              <p>Selected: {selectedQuestion}</p>
+            </div>
+
+            <PrimaryButton onclickFN={() => setCurrentIndex(currentIndex + 1)}>
               Next Question
-            </button>
+            </PrimaryButton>
           </div>
         </section>
       </main>
