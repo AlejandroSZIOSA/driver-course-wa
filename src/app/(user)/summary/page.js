3d65e62ab0dd085cba570cdc407fec95506f0992
AlejandroSZIOSA <ggato33@gmail.com>
2025-03-12 18:16:02 +0100

    summary page tested and ready to use

--- src/app/(user)/summary/page.js
+++ src/app/(user)/summary/page.js
@@ -3,11 +3,12 @@ import SummaryItem from "@/components/summary-item";
 import { useQuestions } from "@/context/QuestionsContext";
 import { useRouter, useSearchParams } from "next/navigation";
 import classes from "@/styles/pages/Summary.module.css";
+import PrimaryButton from "@/components/primary-button";
 
-const QUESTIONS = [
+/* const QUESTIONS = [
   { _id: 1, question: "q1" },
   { _id: 2, question: "q2" },
-];
+]; */
 export default function SummaryPage() {
   const router = useRouter();
   const { questions } = useQuestions();
@@ -23,15 +24,23 @@ export default function SummaryPage() {
       <section>
         <h1> Summary</h1>
         <ol className={classes.listContainer}>
-          {QUESTIONS.map((q) => (
+          {/* fix problem  */}
+          {questions?.map((q) => (
             <li key={q._id}>
               <SummaryItem question={q} userAnswers={userAnswers} />
             </li>
           ))}
         </ol>
         <div>
-          <button onClick={() => router.push("/start")}>Try Again</button>
-          <button onClick={() => router.push("/")}>To Home</button>
+          <PrimaryButton onclickFN={() => router.push("/start")}>
+            Try Again
+          </PrimaryButton>
+          <PrimaryButton onclickFN={() => router.push("/")}>
+            To home
+          </PrimaryButton>
+
+          {/*   <button onClick={() => router.push("/start")}>Try Again</button>
+          <button onClick={() => router.push("/")}>To Home</button> */}
         </div>
       </section>
     </main>
--- src/components/summary-item.js
+++ src/components/summary-item.js
@@ -1,22 +1,30 @@
 "use client";
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import classes from "@/styles/components/SummaryItem.module.css";
 
 export default function SummaryItem({ question, userAnswers }) {
   const [isCorrect, setIsCorrect] = useState(null);
+  const [userAnswer, setUserAnswer] = useState("");
 
   /* debugger; */
   useEffect(() => {
-    setIsCorrect(checkUserAnswer);
+    const [isCorrect, userAnswer] = checkUserAnswer();
+    setIsCorrect(isCorrect);
+    setUserAnswer(userAnswer);
   }, []);
 
   function checkUserAnswer() {
     const { _id, answer } = question;
+    let correctAnswer = answer;
     let isCorrect;
+    let userAnswer;
 
     for (let i = 0; i < userAnswers.length; i++) {
       const { questionId, selectedAnswer } = userAnswers[i];
       if (_id == questionId) {
-        if (answer == selectedAnswer) {
+        userAnswer = selectedAnswer;
+        if (correctAnswer == selectedAnswer) {
           isCorrect = true;
           break;
         } else {
@@ -25,14 +33,31 @@ export default function SummaryItem({ question, userAnswers }) {
         }
       }
     }
-    return isCorrect;
+    return [isCorrect, userAnswer];
+  }
+
+  function showCheckAnswerImage(imageUrl) {
+    return (
+      <Image
+        className={classes.checkImage}
+        src={imageUrl}
+        width={24}
+        height={24}
+        alt="checkImage"
+        priority={true}
+      />
+    );
   }
 
   return (
     <div>
       <h2> 1 {question.question}</h2>
-      <h3> </h3>
-      <p>{isCorrect ? "Correct" : "Wrong"}</p>
+      <div className={classes.answerContainer}>
+        <h3> answer = {userAnswer}</h3>
+        {isCorrect
+          ? showCheckAnswerImage("/svg/icons/checkbox-correct.svg")
+          : showCheckAnswerImage("/svg/icons/checkbox-incorrect.svg")}
+      </div>
     </div>
   );
 }
