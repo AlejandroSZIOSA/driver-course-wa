556531eacaef5c26a8f6857cc49effa915571990
AlejandroSZIOSA <ggato33@gmail.com>
2025-03-14 17:38:56 +0100

    cleaning the code,

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { sanityClient as client } from "@/lib/sanity"; // Ensure you have Sanity client configured
+import { sanityClient as client } from "@/lib/sanity";
 import bcrypt from "bcryptjs";
 
 export async function POST(req) {
