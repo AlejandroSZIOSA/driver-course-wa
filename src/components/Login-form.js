9707068376735b086e6970848c29f0f5c0f4b977
AlejandroSZIOSA <ggato33@gmail.com>
2025-04-09 17:29:41 +0200

    add new user data, new login data with localstorage and protect user data

--- src/components/Login-form.js
+++ src/components/Login-form.js
@@ -7,30 +7,36 @@ export default function LoginForm({ handleUserData }) {
   const [user, setUser] = useState({ email: "", password: "" });
 
   const [showPassword, setShowPassword] = useState(false);
+  const [isLocked, setIsLocked] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
   useEffect(() => {
+    setSavedUserLocalStorage();
+  }, []);
+
+  function setSavedUserLocalStorage() {
     const userStored = localStorage.getItem("user");
     const user = userStored ? JSON.parse(userStored) : null;
     if (user) {
       setUser({ email: user.email, password: user.password });
+      setIsLocked(true);
     }
-  }, []);
-
-  const toggleShowPassword = () => setShowPassword(!showPassword);
+  }
 
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
+  const toggleShowPassword = () => setShowPassword(!showPassword);
+  const toggleLock = () => setIsLocked(!isLocked);
+
   const handleLoginUser = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     setError("");
-
     try {
       const res = await fetch("/api/auth/login", {
         method: "POST",
@@ -44,9 +50,18 @@ export default function LoginForm({ handleUserData }) {
       /* localStorage.setItem("token", data.token); */
 
       if (!res.ok) {
+        if (res.status === 401 || res.status === 404) {
+          localStorage.clear(); //remove current saved user from localstorage
+          setIsLocked(false);
+        }
         setError(data.error);
         setIsLoading(false);
         return;
+      }
+
+      //check if saved user have been removed from localstorage
+      if (localStorage.length === 0) {
+        localStorage.setItem("user", JSON.stringify(user));
       }
 
       setMessage(data.message);
@@ -71,6 +86,7 @@ export default function LoginForm({ handleUserData }) {
           placeholder="Email"
           value={user.email}
           onChange={handleChange}
+          disabled={isLocked}
           required
         />
         <div style={{ display: "flex" }}>
@@ -80,6 +96,7 @@ export default function LoginForm({ handleUserData }) {
             placeholder="Password"
             value={user.password}
             onChange={handleChange}
+            disabled={isLocked}
             required
           />
 
@@ -87,7 +104,9 @@ export default function LoginForm({ handleUserData }) {
             {!showPassword ? "Show" : "Hide"}
           </button>
         </div>
-
+        <button type="button" onClick={toggleLock}>
+          {!isLocked ? "Lock" : "Unlock"}
+        </button>
         <button type="submit">Login</button>
       </form>
     </div>
